89440aa4d7e3e68f2a63431eb5eddc3e5cddfeef
kaji_____ <miurakaji920@gmail.com>
2018-01-24 20:43:57 +0900

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,8 +1,84 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class List extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checked: false,
+    }
+  }
+
+  changeChecked() {
+    this.setState({checked: !this.state.checked})
+  }
+
+  render() {
+    return (
+      <div className='list'>
+        <span onClick={this.changeChecked.bind(this)}>
+          {this.state.checked
+            ? <i className='fas fa-check-circle' />
+            : <i className='far fa-circle' />
+          }
+        </span>
+        {this.props.title}
+      </div>
+    );
+  }
+}
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: ['トイレ掃除', 'ポチの散歩'],
+      inputText: '',
+    }
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+
+    if (this.state.inputText === '') {
+      return
+    }
+
+    const newTasks = this.state.tasks
+    newTasks.push(this.state.inputText)
+    this.setState({
+      tasks: newTasks,
+      inputText: '',
+    })
+  }
+
+  handleChangeText(event) {
+    this.setState({inputText: event.target.value});
+  }
+
+  render() {
+    return (
+      <div className='main'>
+        <h1>タスク一覧</h1>
+        <div className='tasks'>
+          {this.state.tasks.map((task, index) => {
+            return (
+              <List key={`list-${index}`} title={task} />
+            )
+          })}
+        </div>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <input
+            type='text'
+            value={this.state.inputText}
+            onChange={this.handleChangeText.bind(this)}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+ReactDOM.render(
+  <Main />,
+  document.getElementById('root')
+);
